964154a1c0209eeca6c35238e5e12e0288d9ff44
Catherine <katya.simonova.1997@mail.ru>
2023-02-25 02:33:41 +0300

    -

--- Exercise_6.6/Program.cs
+++ Exercise_6.6/Program.cs
@@ -117,7 +117,7 @@ namespace Exercise_6._6
 
 				while ((Line = sr.ReadLine()) != null)
 				{ 
-				string[] data = Line.Split('\t', '#');
+				string[] data = Line.Split('#');
 					WriteLine($"{data[0]} {data[1]} {data[2]} {data[3]} {data[4]} {data[5]} {data[6]} ");
 				}
 			}
